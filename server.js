99fd28498fbbf365978d72b1f8faab1403e1a82f
kamssa <kamssa0@gmail.com>
2020-12-29 03:44:45 +0000

    ajout de express

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/infpa'));
+app.use(express.static('./dist/dashboardImmobilier'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/infpa/'}),
+  res.sendFile('index.html', {root: 'dist/dashboardImmobilier/'}),
 );
 
 app.listen(process.env.PORT || 8080);
